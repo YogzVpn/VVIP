af1d5c658b5069de8c62d1b7e016171799d26456
YogzVpn <136385048+YogzVpn@users.noreply.github.com>
2023-07-02 09:50:07 +0700

    Update menu-ssh.sh

--- update/menu-ssh.sh
+++ update/menu-ssh.sh
@@ -105,7 +105,7 @@ PID=`ps -ef |grep -v grep | grep sshws |awk '{print $2}'`
 
 if [[ ! -z "${PID}" ]]; then
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " ${COLBG1}           • SSH PANEL MENU •           ${NC} "
+echo -e " ${COLBG1}           • SUKSES CREATE BRO •           ${NC} "
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "  Username   : $Login" 
 echo -e "  Password   : $Pass"
@@ -125,7 +125,7 @@ echo -e "  GET wss://bug.com/ HTTP/1.1[crlf]Host: [host] [crlf]Upgrade: websocke
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 else
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " ${COLBG1}            • SSH PANEL MENU •          ${NC} "
+echo -e " ${COLBG1}            • SUKSES CREATE BRO •          ${NC} "
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "  Username   : $Login" 
 echo -e "  Password   : $Pass"
