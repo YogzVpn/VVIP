22b1bf13e2b11740e078bdf06791fc0f949ae585
YogzVpn <136385048+YogzVpn@users.noreply.github.com>
2023-07-06 04:07:01 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -176,9 +176,9 @@ fi
 }
 clear
 clear
-echo -e "${BIWhite}┌────────────────────────────────────────────────────────────┐${NC}"
+echo -e "${BIBlue}┌────────────────────────────────────────────────────────────┐${NC}"
 echo -e "                      << INFORMASI VPS >>                    \E[0m" | lolcat    
-echo -e "${BICyan}└────────────────────────────────────────────────────────────┘${NC}"
+echo -e "${BIBlue}└────────────────────────────────────────────────────────────┘${NC}"
 echo -e "  ${BLUE}• ${GREEN}Sever Uptime        ${NC}= $( uptime -p  | cut -d " " -f 2-10000 ) "
 echo -e "  ${BLUE}• ${GREEN}Current Time        ${NC}= $( date -d "0 days" +"%d-%m-%Y | %X" )"
 echo -e "  ${BLUE}• ${GREEN}Operating System    ${NC}= $( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g')( $(uname -m))"
